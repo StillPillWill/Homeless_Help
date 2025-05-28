c9e1efe2f443b27cf47bd9a9d197f739fabe1d93
SuperSmartGamer <wetwilman20@gmail.com>
2025-05-28 15:21:30 -0700

    more stuff

---
+++ website/src/Components/showMe.js
@@ -0,0 +1,23 @@
+import "../Style.css"
+
+export default function ShowMe(){
+
+var showThing=<>
+<div class="shower">
+    Imagine
+
+</div>
+
+
+</>
+
+return(showThing);
+
+}
+
+export function showBorder(){
+    return(<div class="showerBorder">
+    Imagine
+
+</div>);
+}
--- website/src/pages/Main.js
+++ website/src/pages/Main.js
@@ -1,11 +1,14 @@
 import "../Style.css";
 import NavBar from "../Components/NavBar"; 
 import Background from "../Components/Background";
+import ShowMe from "../Components/showMe";
 
 export default function Main() {
   return (<>
+    <div class="mainContain">
       <NavBar />
-      <Background />
+      <ShowMe />
+      </div>
     </>
     );
 }
